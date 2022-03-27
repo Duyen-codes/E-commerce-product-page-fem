fc1cf22ff48e9b6ca01ae1bf6f477fad0f26cc4c
Duyen Nguyen <hongduyen0705@gmail.com>
2022-03-27 11:18:36 +0300

    implemented slide images next and previous on mobile, layout of main container on desktop

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const nav = document.querySelector("nav");
 const iconClose = document.querySelector(".icon-close");
 const iconCart = document.querySelector(".icon-cart");
 const checkoutCard = document.querySelector(".checkout-card");
-console.log(checkoutCard);
 
 // Toggle mobile nav
 
@@ -21,3 +20,50 @@ iconClose.addEventListener("click", () => {
 iconCart.addEventListener("click", () => {
   checkoutCard.classList.toggle("open");
 });
+
+// Slider
+const slides = document.querySelectorAll(".slide");
+
+const iconNext = document.querySelector(".icon-next");
+const iconPrevious = document.querySelector(".icon-previous");
+
+const slider = document.querySelector(".slider");
+
+// slides.forEach((slide, index) => {
+//   slide.style.transform = `translateX(${100 * index}%)`;
+// });
+
+let currentSlide = 0;
+let maxSlide = slides.length - 1;
+
+const goToSlide = function (slide) {
+  slides.forEach((sl, index) => {
+    sl.style.transform = `translateX(${100 * (index - slide)}%)`;
+  });
+};
+
+goToSlide(0);
+
+// Next slide
+const nextSlide = function () {
+  if (currentSlide === maxSlide) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+  goToSlide(currentSlide);
+};
+
+const prevSlide = function () {
+  if (currentSlide === 0) {
+    currentSlide = maxSlide;
+  } else {
+    currentSlide--;
+  }
+  goToSlide(currentSlide);
+};
+
+iconNext.addEventListener("click", nextSlide);
+iconPrevious.addEventListener("click", prevSlide);
+
+// currentSlide = 1: -100%, 0%, 100%, 200%
